f6ac7e54e093dff50ee35c1427f6d3dfaf9ebefd
Onyinbrakeme Kelvin Ogbe <35059691+Oyinss@users.noreply.github.com>
2026-08-23 18:50:32 +0100

    🧹 Chore: remove entire waybar configuration and scripts

--- config/waybar/scripts/Cava.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-exec cava -p "$HOME/.config/cava/config1" 2>/dev/null \
-  | sed -u 's/;//g;s/0/▁/g;s/1/▂/g;s/2/▃/g;s/3/▄/g;s/4/▅/g;s/5/▆/g;s/6/▇/g;s/7/█/g;'
--- config/waybar/scripts/rofi-toggle.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if pgrep -x rofi > /dev/null; then
-    pkill -x rofi
-else
-    rofi -show drun -replace
-fi
