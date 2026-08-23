bfb390c4c3af549f0c574dac8cdd0f7e7bf09a0c
Onyinbrakeme Kelvin Ogbe <35059691+Oyinss@users.noreply.github.com>
2026-08-23 08:21:08 +0100

    🎨 Update color configurations for Hyprland and Hyprlock

--- config/hypr/hyprland/colors.lua
+++ config/hypr/hyprland/colors.lua
@@ -1,16 +1,16 @@
 hl.config({
     general = {
         col = {
-            active_border   = "rgba(49464877)",
-            inactive_border = "rgba(1d1b1c33)",
+            active_border   = "rgba(324b4977)",
+            inactive_border = "rgba(111e1d33)",
         },
     },
     misc = {
-        background_color = "rgba(141314FF)",
+        background_color = "rgba(091615FF)",
     },
 })
 
 hl.window_rule({
     match        = { pin = 1 },
-    border_color = "rgba(d1c2d2AA) rgba(d1c2d277)",
+    border_color = "rgba(97d782AA) rgba(97d78277)",
 })
